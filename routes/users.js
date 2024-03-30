ff659c6db54167618fe789412f21538ca7f3f5db
Joey585 <thelich06@protonmail.com>
2024-03-29 23:34:37 -0700

    New endpoint
    receive a certain amount of uploads from user

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,7 @@ const authCheck = require("../middleware/authCheck");
 const domainModel = require("../schema/domain");
 const userModel = require("../schema/user");
 const isAdmin = require("../util/isKeyAdmin");
+const uploadModel = require("../schema/upload");
 
 /* GET users listing. */
 router.get('/@me', authCheck, function(req, res, next) {
@@ -32,6 +33,20 @@ router.get("/isAdmin", authCheck, (req, res) => {
     if(isAdmin(req.user.apiKey)) return res.status(200).json({status: 200, d: {result: true}});
 
     return res.status(200).json({status: 200, d: {result: false}});
-})
+});
+
+router.get("/@me/uploads/get/:amount", authCheck, async (req, res) => {
+    if(!req.params.amount) return res.status(400).json({status: 400, error: "Bad request, must include amount"});
+    if(!parseInt(req.params.amount)) return res.status(400).json({status: 400, error: "Amount is not a number"});
+    if(req.params.amount > 100) return res.status(400).json({status: 400, error: "Too big of a fetch"});
+    console.log({username: req.user.username})
+    const images = await uploadModel.find({userUploaded: req.user.username}).sort({dateCreated: 'descending'}).limit(req.params.amount);
+
+    if(images){
+        return res.status(200).json({status: 200, d: images});
+    }
+
+    return res.status(500).json({status: 500, error: "Internal Server Error"})
+});
 
 module.exports = router;
